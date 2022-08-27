095429ad79ad7abc23525b05c36dafff627d44cc
Tristan Ravitch <tristan@ravit.ch>
2022-08-26 19:47:14 -0700

    bootstrap: Try to avoid rustup path modifications (again)

--- home/Scripts/bootstrap.sh
+++ home/Scripts/bootstrap.sh
@@ -149,7 +149,7 @@ RUSTUP=$HOME/.cargo/bin/rustup
 install_rustup() {
     if [ ! -x "${RUSTUP}" ]
     then
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh - --no-modify-path
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path
     fi
 }
 
