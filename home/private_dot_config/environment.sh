13c38fd06c5c3edb10ad248141cdd0a478e14923
Tristan Ravitch <tristan@ravit.ch>
2022-09-17 16:41:25 -0700

    environment: Add a path for Homebrew

--- home/private_dot_config/environment.sh
+++ home/private_dot_config/environment.sh
@@ -1,7 +1,7 @@
 # Environment settings shared across multiple machines
 
 EMACS=/usr/local/emacs/current/bin
-export PATH=$EMACS:$HOME/.local/bin:$HOME/.ghcup/bin:$HOME/.cargo/bin:$HOME/.cabal/bin:/usr/local/bin:/usr/bin:/bin
+export PATH=$EMACS:$HOME/.local/bin:$HOME/.ghcup/bin:$HOME/.cargo/bin:$HOME/.cabal/bin:$HOME/.homebrew/bin:/usr/local/bin:/usr/bin:/bin
 export EDITOR='emacs -nw'
 export CLICOLOR=1
 
