fa691fcc4344a039ce17491d3a731da7ba5604c4
Tristan Ravitch <tristan@ravit.ch>
2022-09-28 20:37:15 -0700

    emacs: Revert lsp/plist change
    It didn't seem to work in practice. Running in this configuration yielded
    thousands of errors where emacs expected a hash table.

--- home/dot_emacs.d/init.el
+++ home/dot_emacs.d/init.el
@@ -359,7 +359,6 @@
   :init
   (setq gc-cons-threshold 100000000)
   (setq read-process-output-max (* 1024 1024))
-  (setq lsp-use-plists t)
   (setq lsp-log-io nil)
   :config
   (add-hook 'java-mode-hook #'(lambda () (when (eq major-mode 'java-mode) (lsp-deferred))))
--- home/private_dot_config/environment.sh
+++ home/private_dot_config/environment.sh
@@ -6,11 +6,6 @@ export PATH=$EMACS:$HOME/.local/bin:$HOME/.ghcup/bin:$HOME/.cargo/bin:$HOME/.cab
 export EDITOR='emacs -nw'
 export CLICOLOR=1
 
-# Configuration for emacs LSP that causes it to use a more efficient serialization mechanism
-#
-# This must be set when building lsp-mode
-export LSP_USE_PLISTS=true
-
 if [[ ! -v SSH_AUTH_SOCK ]];
 then
 
