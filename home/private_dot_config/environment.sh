bd2b19dfd1b798a1ed025194e3fa9ed2f68fd0d9
Tristan Ravitch <tristan@ravit.ch>
2023-07-16 23:22:19 -0700

    env: More homebrew paths

--- home/private_dot_config/environment.sh
+++ home/private_dot_config/environment.sh
@@ -3,8 +3,9 @@
 
 # Select the most recent emacs
 EMACS=/usr/local/emacs/current/bin
+HOMEBREW=$HOME/.homebrew/bin:/opt/homebrew/bin
 
-export PATH=$EMACS:$HOME/.local/bin:$HOME/.ghcup/bin:$HOME/.cargo/bin:$HOME/.cabal/bin:$HOME/.homebrew/bin:/usr/local/bin:/usr/bin:/bin:$HOME/Scripts
+export PATH=$EMACS:$HOME/.local/bin:$HOME/.ghcup/bin:$HOME/.cargo/bin:$HOMEBREW:$HOME/.cabal/bin:/usr/local/bin:/usr/bin:/bin:$HOME/Scripts
 export EDITOR='emacs -nw'
 export CLICOLOR=1
 
