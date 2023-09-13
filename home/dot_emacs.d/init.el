7b17f9bc2c321fe977b020e6e04d688a9604d5aa
Tristan Ravitch <tristan@malaz.local>
2023-09-12 23:24:15 -0600

    emacs: Load the theme in the elpaca after-init hook

--- home/dot_emacs.d/init.el
+++ home/dot_emacs.d/init.el
@@ -223,10 +223,12 @@
 ;; Use solarized light, and turn down some of the more aggressive font choices
 ;; that (especially a few that are proportional).
 (use-package solarized-theme
+  :hook (elpaca-after-init . (lambda () (load-theme 'solarized-light t)))
   :config
   (setq solarized-use-variable-pitch nil)
-  (setq solarized-scale-org-headlines nil)
-  (load-theme 'solarized-light t))
+  (setq solarized-scale-org-headlines nil))
+
+;  (load-theme 'solarized-light t))
 
 ;; This is a modeline replacement that is a bit cleaner while still being lightweight (compared to e.g., spaceline)
 (use-package simple-modeline
