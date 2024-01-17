de29f462d7fdc65af0fd65b184221fa93b41975a
Tristan Ravitch <tristan@ravit.ch>
2024-01-16 20:35:07 -0700

    fish: Initial configuration

---
+++ home/private_dot_config/environment.fish
@@ -0,0 +1,17 @@
+set -gx EMACS /usr/local/emacs/current/bin
+set -gx HOMEBREW $HOME/.homebrew/bin:/opt/homebrew/bin
+set -gx GHC $HOME/.ghcup/bin
+set -gx CABAL $HOME/.cabal/bin
+set -gx CARGO $HOME/.cargo/bin
+set -gx Z3 $HOME/Tools/Z3/current/bin
+set -gx CVC5 $HOME/Tools/CVC5/current/bin
+
+set -gx EDITOR 'emacs -nw'
+set -gx PATH $EMACS:$HOME/Scripts:$CARGO:$HOME/.local/bin:$Z3:$CVC5:$HOMEBREW:$GHC:$CABAL:/usr/local/bin:/usr/bin
+set -gx CLICOLOR 1
+
+set LOCAL_ENV $HOME/.config/local-environment.fish
+
+if test -f $LOCAL_ENV
+    source $LOCAL_ENV
+end
---
+++ home/private_dot_config/private_fish/config.fish
@@ -0,0 +1,8 @@
+source $HOME/.config/environment.fish
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -U fish_greeting
+
+    starship init fish | source
+end
