d3480fc2c2ac4d402af37980385c23380c355332
Tristan Ravitch <tristan@ravit.ch>
2024-01-21 17:02:42 -0700

    environment: Make helper variables local

--- home/private_dot_config/environment.fish
+++ home/private_dot_config/environment.fish
@@ -1,10 +1,10 @@
-set -gx EMACS /usr/local/emacs/current/bin
-set -gx HOMEBREW $HOME/.homebrew/bin:/opt/homebrew/bin
-set -gx GHC $HOME/.ghcup/bin
-set -gx CABAL $HOME/.cabal/bin
-set -gx CARGO $HOME/.cargo/bin
-set -gx Z3 $HOME/Tools/Z3/current/bin
-set -gx CVC5 $HOME/Tools/CVC5/current/bin
+set -l EMACS /usr/local/emacs/current/bin
+set -l HOMEBREW $HOME/.homebrew/bin:/opt/homebrew/bin
+set -l GHC $HOME/.ghcup/bin
+set -l CABAL $HOME/.cabal/bin
+set -l CARGO $HOME/.cargo/bin
+set -l Z3 $HOME/Tools/Z3/current/bin
+set -l CVC5 $HOME/Tools/CVC5/current/bin
 
 set -gx EDITOR 'emacs -nw'
 set -gx PATH $EMACS:$HOME/Scripts:$CARGO:$HOME/.local/bin:$Z3:$CVC5:$HOMEBREW:$GHC:$CABAL:/bin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin
