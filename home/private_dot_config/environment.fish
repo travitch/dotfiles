3978478c35041cd32611a6c38151d1f7a155ec95
Tristan Ravitch <tristan@ravit.ch>
2024-04-08 23:20:49 -0600

    fish: Do not export some helper variables

--- home/private_dot_config/environment.fish
+++ home/private_dot_config/environment.fish
@@ -1,12 +1,12 @@
-set -l EMACS /usr/local/emacs/current/bin
-set -l HOMEBREW $HOME/.homebrew/bin:/opt/homebrew/bin
-set -l GHC $HOME/.ghcup/bin
-set -l CABAL $HOME/.cabal/bin
-set -l CARGO $HOME/.cargo/bin
-set -l Z3 $HOME/Tools/Z3/current/bin
-set -l CVC5 $HOME/Tools/CVC5/current/bin
-set -l DAFNY $HOME/Tools/Dafny/4.4.0
-set -l MISE $HOME/.local/share/mise/shims
+set -u EMACS /usr/local/emacs/current/bin
+set -u HOMEBREW $HOME/.homebrew/bin:/opt/homebrew/bin
+set -u GHC $HOME/.ghcup/bin
+set -u CABAL $HOME/.cabal/bin
+set -u CARGO $HOME/.cargo/bin
+set -u Z3 $HOME/Tools/Z3/current/bin
+set -u CVC5 $HOME/Tools/CVC5/current/bin
+set -u DAFNY $HOME/Tools/Dafny/4.4.0
+set -u MISE $HOME/.local/share/mise/shims
 
 set -gx EDITOR 'emacs -nw'
 set -gx PATH $EMACS:$HOME/Scripts:$CARGO:$MISE:$HOME/.local/bin:$Z3:$CVC5:$HOMEBREW:$GHC:$CABAL:$DAFNY:/bin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin
