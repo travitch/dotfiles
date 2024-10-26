aa92d1a0e6c94b9a8f8a27bfeb22cc9ab6b7fd77
Tristan Ravitch <tristan@ravit.ch>
2024-10-26 00:55:37 -0600

    emacs: Switch to lambda-line

--- home/dot_emacs.d/init.el
+++ home/dot_emacs.d/init.el
@@ -257,8 +257,30 @@
   :hook (after-init . (lambda () (load-theme 'modus-operandi-tinted))))
 
 ;; This is a modeline replacement that is a bit cleaner while still being lightweight (compared to e.g., spaceline)
-(use-package simple-modeline
-  :hook (after-init . simple-modeline-mode))
+;; (use-package simple-modeline
+;;   :hook (after-init . simple-modeline-mode))
+
+(use-package lambda-line
+  :straight (:type git :host github :repo "lambda-emacs/lambda-line")
+  :custom
+  (lambda-line-position 'top) ;; Set position of status-line
+  (lambda-line-abbrev t) ;; abbreviate major modes
+  (lambda-line-hspace "  ")  ;; add some cushion
+  (lambda-line-prefix t) ;; use a prefix symbol
+  (lambda-line-prefix-padding nil) ;; no extra space for prefix
+  (lambda-line-status-invert nil)  ;; no invert colors
+  (lambda-line-gui-ro-symbol  " ⨂") ;; symbols
+  (lambda-line-gui-mod-symbol " ⬤")
+  (lambda-line-gui-rw-symbol  " ◯")
+  (lambda-line-space-top +.50)  ;; padding on top and bottom of line
+  (lambda-line-space-bottom -.50)
+  (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
+  :hook (after-init . lambda-line-mode)
+  :config
+  ;; set divider line in footer
+  (when (eq lambda-line-position 'top)
+    (setq-default mode-line-format (list "%_"))
+    (setq mode-line-format (list "%_"))))
 
 ;; This mode provides a function that enables a server running from emacs that
 ;; can edit text boxes in browsers using an appropriate extension.
