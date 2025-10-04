ad79339e0fb6fef7391d1dfd27d0c62efcdf2944
Tristan Ravitch <tristan@ravit.ch>
2025-10-04 17:04:36 -0600

    wezterm: Switch theme to ef-light

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -3,14 +3,8 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback { 'MonoLisa', 'Noto Color Emoji' }
--- config.color_scheme = 'Builtin Solarized Light'
--- config.color_scheme = 'Violet Light'
--- config.color_scheme = 'Alabaster'
-config.color_scheme = 'Night Owlish Light'
--- config.color_scheme = 'ayu_light'
--- config.color_scheme = 'dayfox'
--- config.color_scheme = 'Solarized Light (Gogh)'
--- config.color_scheme = 'Modus-Operandi'
+-- config.color_scheme = 'Night Owlish Light'
+config.color_scheme = 'Ef-Light'
 
 config.enable_tab_bar = false
 config.term = 'wezterm'
