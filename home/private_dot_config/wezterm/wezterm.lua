19af42b42737d8d07ae266ea642c0e541426c696
Tristan Ravitch <tristan@ravit.ch>
2026-02-15 12:22:51 -0700

    font: Switch from MonoLisa to Maple Mono

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font_with_fallback { 'MonoLisa', 'Noto Color Emoji' }
+-- config.font = wezterm.font_with_fallback { 'MonoLisa', 'Noto Color Emoji' }
+config.font = wezterm.font_with_fallback { 'Maple Mono NF', 'Noto Color Emoji' }
 -- config.color_scheme = 'Night Owlish Light'
 config.color_scheme = 'Ef-Light'
 
