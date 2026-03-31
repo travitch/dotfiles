3ab6abd3cc9be4fda4e6f36c23a5d86b1eb09e12
Tristan Ravitch <tristan@ravit.ch>
2026-03-30 21:25:26 -0600

    wezterm: Switch the font to Monaspace Argon

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -2,8 +2,13 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.font = wezterm.font {
+   family = 'Monaspace Argon NF',
+   harfbuzz_features = {'cv01=2', 'liga', 'ss07'},
+}
+
 -- config.font = wezterm.font_with_fallback { 'MonoLisa', 'Noto Color Emoji' }
-config.font = wezterm.font_with_fallback { 'Maple Mono NF', 'Noto Color Emoji' }
+-- config.font = wezterm.font_with_fallback { 'Maple Mono NF', 'Noto Color Emoji' }
 -- config.color_scheme = 'Night Owlish Light'
 config.color_scheme = 'Ef-Light'
 
