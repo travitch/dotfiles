2d9df9d70ded6cb18568c43e10bb894170b7e721
Tristan Ravitch <tristan@ravit.ch>
2026-06-10 21:54:47 -0600

    emacs: Minor cleanups

---
+++ home/dot_emacs.d/custom.el
@@ -0,0 +1,30 @@
+;; -*- lexical-binding: t; -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("ee24217382fe1dea821e91298dfc5c9c3cb46a553fc9720d7c232c7fdfc029a6"
+     "3468e63fa8ec3c767d755f2c077e680ec9583abf01a478107a245c5e4e224b92"
+     "6b9fbe5d88424ac7283b8f36b6f184d1140fcd4bfcab1f72a3c58c48dc254bae"
+     "664111db1521fe3351061dc87aea95fa98b3f244f4b830fbc048d39c3a8bc125"
+     "3d9512412d5874972f9142a6c230258e33ff1168d1d21aa68d7a568f328a400b"
+     "7e98dc1aa7f5db0557691da690c38d55e83ddd33c6d268205d66e430d57fb982"
+     "0592f1b03ba5d7d8cfade2ce3a23db4c0d5f9926c9ae918dd740739dd95b697c"
+     "c3076fdee603e9768817cfe8dbf6253d5b3cf3bf4602cb32fa2f1df62fe70b1c"
+     "5e39e95c703e17a743fb05a132d727aa1d69d9d2c9cde9353f5350e545c793d4"
+     "6819104c5f7d70485b32c10323aa396806d282fcee5b707e462bf3d156f44c39"
+     "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3"
+     "c038d994d271ebf2d50fa76db7ed0f288f17b9ad01b425efec09519fa873af53"
+     "fbf73690320aa26f8daffdd1210ef234ed1b0c59f3d001f342b9c0bbf49f531c"
+     "712dda0818312c175a60d94ba676b404fc815f8c7e6c080c9b4061596c60a1db"
+     "99d1e29934b9e712651d29735dd8dcd431a651dfbe039df158aa973461af003e"
+     "c1638a7061fb86be5b4347c11ccf274354c5998d52e6d8386e997b862773d1d2" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
