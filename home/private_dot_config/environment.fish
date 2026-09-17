adebf5c0a104a5bcbf6790134f0bee4949d9ef29
Tristan Ravitch <tristan@ravit.ch>
2026-09-16 22:02:27 -0600

    env: Add Linux homebrew paths

--- home/private_dot_config/environment.fish
+++ home/private_dot_config/environment.fish
@@ -1,24 +1,17 @@
 set -u EMACS /usr/local/emacs/current/bin
-set -u HOMEBREW $HOME/.homebrew/bin:/opt/homebrew/bin
-set -u GHC $HOME/.ghcup/bin
-set -u CABAL $HOME/.cabal/bin
+set -u HOMEBREW $HOME/.homebrew/bin:/opt/homebrew/bin:/home/linuxbrew/.linuxbrew/bin
 set -u CARGO $HOME/.cargo/bin
 set -u Z3 $HOME/Tools/Z3/current/bin
 set -u CVC5 $HOME/Tools/CVC5/current/bin
-set -u DAFNY $HOME/Tools/Dafny/4.4.0
 set -u ELAN $HOME/.elan/bin
 set -u LSPS $HOME/.emacs.d/mason/bin
 
 set -gx EDITOR 'emacs -nw'
-set -gx PATH $EMACS:$LSPS:$HOME/Scripts:$CARGO:$ELAN:$HOME/.local/bin:$Z3:$CVC5:$HOMEBREW:$GHC:$CABAL:$DAFNY:/bin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin
+set -gx PATH $EMACS:$LSPS:$HOME/Scripts:$CARGO:$ELAN:$HOME/.local/bin:$Z3:$CVC5:$HOMEBREW:/bin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin
 set -gx CLICOLOR 1
 set -gx COLORTERM truecolor
 
 alias e 'emacs -nw'
-
-# if type -q mise
-#     mise activate fish | source
-# end
 
 # On Linux, set the SSH agent socket
 if test -d /etc/xdg
