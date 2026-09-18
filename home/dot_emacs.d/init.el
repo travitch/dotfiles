40491b7df70532e6d4298a413d2a7dbde994d4cd
Tristan Ravitch <tristan@ravit.ch>
2026-09-17 23:20:06 -0600

    emacs: Remove mason, using homebrew for LSP installations instead

--- home/dot_emacs.d/init.el
+++ home/dot_emacs.d/init.el
@@ -495,32 +495,6 @@
   :ensure t
   :mode ("\\.ninja$" . ninja-mode))
 
-(defconst +tr/mason-lsps '(bash-language-server
-                           clangd
-                           fish-lsp
-                           gopls
-                           smithy-language-server
-                           marksman
-                           jdtls
-                           basedpyright
-                           zuban
-                           java-debug-adapter
-                           typescript-language-server))
-
-(use-package mason
-  :ensure t
-  :init
-  (defun tr/install-lsps ()
-    "Install required LSPs."
-    (mason-ensure
-     (lambda ()
-       (seq-doseq (pkg +tr/mason-lsps)
-         (unless (mason-installed-p pkg)
-           (ignore-errors (mason-install (symbol-name pkg))))))))
-  :functions (mason-ensure mason-install mason-installed-p)
-  :hook
-  (elpaca-after-init-hook . tr/install-lsps))
-
 ;; JDTLS replies with non-standard file URLs in some cases.  This code handles them.
 ;;
 ;; Taken from https://gitlab.com/skybert/my-little-friends/-/blob/master/emacs/.emacs.d/tkj-java-eglot.el
--- home/private_dot_config/environment.fish
+++ home/private_dot_config/environment.fish
@@ -1,13 +1,11 @@
 set -u EMACS /usr/local/emacs/current/bin
 set -u HOMEBREW $HOME/.homebrew/bin:/opt/homebrew/bin:/home/linuxbrew/.linuxbrew/bin
 set -u CARGO $HOME/.cargo/bin
-set -u Z3 $HOME/Tools/Z3/current/bin
 set -u CVC5 $HOME/Tools/CVC5/current/bin
 set -u ELAN $HOME/.elan/bin
-set -u LSPS $HOME/.emacs.d/mason/bin
 
 set -gx EDITOR 'emacs -nw'
-set -gx PATH $EMACS:$LSPS:$HOME/Scripts:$CARGO:$ELAN:$HOME/.local/bin:$Z3:$CVC5:$HOMEBREW:/bin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin
+set -gx PATH $EMACS:$HOME/Scripts:$CARGO:$ELAN:$HOME/.local/bin:$CVC5:$HOMEBREW:/bin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin
 set -gx CLICOLOR 1
 set -gx COLORTERM truecolor
 
